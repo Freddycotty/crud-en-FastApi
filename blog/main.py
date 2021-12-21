8dac42265887edb5039726ad5a617108c73a9c0e
freddycotty <freddycarrillo1912@gmail.com>
2021-12-21 14:01:13 -0400

    [ADD] Creando Usuario

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,6 @@
 from typing import List
 from fastapi import FastAPI, Depends, status, Response, HTTPException
-from .schemas import Blog, ShowBlog
-from blog import models
+from blog import models, schemas
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
 app = FastAPI()
@@ -18,7 +17,7 @@ def get_db():
 
 
 @app.post('/blog', status_code=201)
-def create(request: Blog, db: Session = Depends(get_db)):
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
@@ -26,13 +25,13 @@ def create(request: Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
-@app.get('/blog', status_code=200, response_model=List[ShowBlog])
+@app.get('/blog', status_code=200, response_model=List[schemas.ShowBlog])
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code=200, response_model=ShowBlog)
+@app.get('/blog/{id}', status_code=200, response_model=schemas.ShowBlog)
 def show(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -53,7 +52,7 @@ def destroy(id, db: Session = Depends(get_db)):
 
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: Blog, db: Session = Depends(get_db)):
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -61,3 +60,16 @@ def update(id, request: Blog, db: Session = Depends(get_db)):
     blog.update(request.dict())
     db.commit()
     return 'updated'
+
+
+@app.post('/user')
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name,
+        email=request.email,
+        password=request.password
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
